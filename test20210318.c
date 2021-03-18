0a423e49fef8df5a6eafd1c549c845ccce1ee1f2
daydayup-ljq <2177493804@qq.com>
2021-03-18 22:20:57 +0800

    大二结构体(五)

---
+++ test20210318.c
@@ -0,0 +1,54 @@
+﻿#define _CRT_SECURE_NO_WORINGS
+#define _CRT_SECURE_NO_WARNINGS
+#define MAX 10
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+typedef struct linknode
+{
+	int date;
+	struct linknode* next;
+
+
+}Linknode;
+
+Linknode* createLinkList()
+{
+	Linknode* h = NULL;
+	Linknode* p = NULL;
+	int x;
+
+	scanf("%d",&x);
+
+	while (x != 0)
+	{
+		p = (Linknode*)malloc(sizeof(Linknode));
+		p->date = x;
+		p->next = h;
+		h = p;
+
+		scanf("%d", &x);
+	}
+	return h;
+}
+
+void showLinkList(Linknode* h)
+{
+	Linknode* p = h;
+	printf("LinkList is :\n");
+	while (p != NULL)
+	{
+		printf("%d ", p->date);
+		p = p->next;
+	}
+
+	printf("\n");
+}
+int main()
+{
+	Linknode* H = createLinkList();
+	showLinkList(H);
+	return 0;
+}
